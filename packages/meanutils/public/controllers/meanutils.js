3f5a7568f2531faab6bdcfe89b8032b7ba1669fb
paragwaghela <parag@highground.com>
2015-03-06 12:12:35 +0530

    marge develop

--- packages/meanutils/public/controllers/meanutils.js
+++ packages/meanutils/public/controllers/meanutils.js
@@ -1,35 +1,35 @@
 'use strict';
 
-<<<<<<< HEAD
+
 angular.module('mean.meanutils').controller('MeanutilsController', ['$scope', 'Global', 'Meanutils','ProjectService',
     function($scope, Global, Meanutils, projectService) {
-=======
-angular.module('mean.meanutils').controller('MeanutilsController', ['$scope', 'Global', 'MenuService',
-    function($scope, Global, MenuService) {
->>>>>>> ea6c5b14f390f946a135af9f5e23b2efe78ddde1
         $scope.global = Global;
         $scope.package = {
             name: 'meanutils'
         };
-<<<<<<< HEAD
+
         $scope.menuUtil = [ {title: 'Project', link:'#!/meanutils/example/project'},
                             {title:'User', link: ''},
                             {title: 'Add project',link: '#!/meanutils/example/project/add' }
                           ];
-        $scope.addProject = function(isValid){
-            if(isValid) {
+        $scope.addProject = function(isValid) {
+            if (isValid) {
                 var project = new projectService({
                     title: this.title,
-                    deadline :this.deadline,
+                    deadline: this.deadline,
                     discription: this.discription,
-                    price : this.price
+                    price: this.price
                 });
-               console.log(project);
-                project.$save(function(response) {
+                console.log(project);
+                project.$save(function (response) {
                     $location.path('meanutils/example/project/');
                 });
-=======
-
+                this.title = '';
+                this.discription = '';
+            } else {
+                $scope.submitted = true;
+            }
+        };
         MenuService.query(function(menu) {
             console.log('Inside----');
             console.log(menu[0].menuName);
@@ -37,15 +37,6 @@ angular.module('mean.meanutils').controller('MeanutilsController', ['$scope', 'G
             $scope.menuUtil = [ {title: menu[0].menuName, subMenu: menu[0].submenu}];
         });
 
-        $scope.all = function(){
->>>>>>> ea6c5b14f390f946a135af9f5e23b2efe78ddde1
-
-                this.title = '';
-                this.discription = '';
-            } else {
-                $scope.submitted = true;
-            }          ;
-        }
         $scope.all = function(){
             projectService.query(function(projects) {
                $scope.projects = projects;
